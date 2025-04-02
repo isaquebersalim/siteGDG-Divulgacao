b5372fc1b0e962a98d44f7b12b28b0c631ce5742
Isaque Bersalim <isaquebersalim@gmail.com>
2025-04-02 01:10:55 -0300

    Implementado JavaScript na página do formulário

---
+++ scriptForm.js
@@ -0,0 +1,29 @@
+document.getElementById("inscricaoForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const nome = document.getElementById("nome").value.trim();
+    const email = document.getElementById("email").value.trim();
+
+    if (nome === "" || email === "") {
+        alert("Preencha todos os campos.");
+        return;
+    }
+
+    const idGerado = "GDG-" + Math.floor(1000 + Math.random() * 9000);
+    document.getElementById("resultado").innerHTML = 
+        `Olá, ${nome}! Seu ID de inscrição é <strong>${idGerado}</strong>. 
+        Um email com seu ID foi enviado para ${email}.`;
+
+    // Exibir contador e iniciar a contagem regressiva
+    document.getElementById("timer").classList.remove("hidden");
+    let tempo = 20;
+    const contador = document.getElementById("contador");
+
+    const intervalo = setInterval(() => {
+        tempo--;
+        contador.textContent = tempo;
+        if (tempo === 0) {
+            clearInterval(intervalo);
+            window.location.href = "index.html"; // Redireciona para a página inicial
+        }
+    }, 1000);
+});
